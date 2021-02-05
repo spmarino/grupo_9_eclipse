386863198e5d835972b7c4a6d57a34c82f281c9a
Gise Montania <gisemontania@gmail.com>
2021-02-04 21:11:33 -0300

    edito

--- site/app.js
+++ site/app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var carritoRouter = require('./routes/carrito');
 
+var carritoRouter = require('./routes/carrito');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
