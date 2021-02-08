ee87cfa1910ae32c7b6f64b8f52d9da9fcb91772
gabriel Ayala <gabriel.ayalagimenez@gmail.com>
2021-02-08 17:44:07 -0300

    creacion registro/login

--- site/app.js
+++ site/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/loginRouter')
+var registerRouter = require ('./routes/registerRouter')
+
 
 var app = express();
 
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter)
+app.use('/register',registerRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
