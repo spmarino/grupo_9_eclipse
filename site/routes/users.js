8eb96e7aef12f8f7887232db9bc3e2386931548b
spmarino <spmarino@hotmail.com>
2021-02-26 22:33:30 -0300

    cambios generales

--- site/routes/users.js
+++ site/routes/users.js
@@ -2,12 +2,6 @@ var express = require('express');
 var router = express.Router();
 const userController = require ('../controllers/userController')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-
 router.get ('/login', userController.login)
 
 module.exports = router;
