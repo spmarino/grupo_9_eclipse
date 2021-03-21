7b3738b60d88853bc1d9f8b70d011a07cce9f179
spmarino <spmarino@hotmail.com>
2021-03-21 01:42:49 -0300

    instalando express

--- site/app.js
+++ site/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const methodOverride = require("method-override");
 var session = require('express-session');
-
+const dbConnectionTest = require('./utils/dbConnectionTest');
+require('dotenv').config();
 
 var indexRouter = require('./routes/indexRouter');
 var carritoRouter = require('./routes/carritoRouter');
@@ -15,6 +16,7 @@ var ingresoRouter = require('./routes/ingresoRouter');
 var cookieCheck = require('./middlewares/cookieCheck')
 
 var app = express();
+dbConnectionTest();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
