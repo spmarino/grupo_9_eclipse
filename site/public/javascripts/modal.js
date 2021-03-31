fadef43676462921d62acdb57289dd5c2b2c14ed
spmarino <spmarino@hotmail.com>
2021-03-31 01:45:28 -0300

    cambios esteticos

---
+++ site/public/javascripts/modal.js
@@ -0,0 +1,33 @@
+let cerrar = document.querySelectorAll(".close2")[0];
+
+let modal = document.querySelectorAll(".modal")[0];
+let modalC = document.querySelectorAll(".modal-container")[0];
+
+
+addEventListener("load", function (e) {
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click", function () {
+    modal.classList.toggle("modal-close");
+    modalC.style.opacity = "0";
+    modalC.style.visibility = "hidden";
+    setTimeout(function(){
+
+    },850)
+})
+
+window.addEventListener("click", function(e){
+   
+    if(e.target == modalC){
+        modal.classList.toggle("modal-close");
+        modalC.style.opacity = "0";
+        modalC.style.visibility = "hidden";
+        setTimeout(function(){
+    
+        },900)
+    }
+})
