8ec1909862e27f2a0a74265ad5e7ff970dca5eb7
spmarino <spmarino@hotmail.com>
2021-04-02 03:22:03 -0300

    agrega productos, agrega tabla a usuarios

--- site/prueba.js
+++ site/prueba.js
@@ -1,31 +1,41 @@
-<% vintosTintos.forEach(product=> { %>
-    <section class="col-5 col-md-3 col-lg-2 producto-box">
-      <figure class="imagen-producto">
-        <img src="/images/<%- product.image %> " alt="">
-      </figure>
-      <% if (product.free_shipping> 0) { %>
-        <div class="envio-promo"><span>Envio gratis</span></div>
-        <% } %>
-          <% if (product.discount> 0) { %>
-            <div class="descuento"><span>
-                <%- product.discount %> % OFF
-              </span></div>
-            <% } %>
-              <article class="datos-articulo">
-                <h2>
-                  <%-product.title%>
-                </h2>
-                <div class="precio">
-                  <% if (product.discount> 0) { %>
-                  <span class="precio-real">$<%- toThousand(product.price) %> </span>
-                  <% } %>
-                  <span class="precio-desc">$<%- toThousand(product.price - Math.round(product.price / 100 * product.discount)) %> </span>
-                </div>
-                <div class="button-agregar">
-                  <form action="/products/detail/<%- product.id %>" method="GET">
-                  <button type="submit" class="button-compra">Agregar</button>
-                </form>
-                </div>
-              </article>
-    </section>
-    <% }) %>
+let cerrar = document.querySelectorAll(".close2")[0];
+let aceptar = document.querySelectorAll(".acept")[0];
+let modal = document.querySelectorAll(".modal")[0];
+let modalC = document.querySelectorAll(".modal-container")[0];
+
+
+
+let cerrar2 = document.querySelectorAll(".close22")[0];
+
+let modal2 = document.querySelectorAll(".modal2")[0];
+let modalC2 = document.querySelectorAll(".modal-container2")[0];
+
+
+
+
+
+addEventListener("load", function (e) {
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+});
+
+aceptar.addEventListener("click", function () {
+    modal.classList.toggle("modal-close");
+    modalC.style.opacity = "0";
+    modalC.style.visibility = "hidden";
+    setTimeout(function(){
+
+    },850)
+})
+
+cerrar.addEventListener("click", function () {
+    
+    modalC2.style.opacity = "1";
+    modalC2.style.visibility = "visible";
+    modal2.classList.toggle("modal-close22");
+    modal.classList.toggle("modal-close");
+
+
+})
