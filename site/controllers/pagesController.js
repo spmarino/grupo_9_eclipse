cee8c401934114dae54909dcad1f81dde90dedb6
spmarino <spmarino@hotmail.com>
2021-04-08 23:24:58 -0300

    agrego dos paginas mas del footer

--- site/controllers/pagesController.js
+++ site/controllers/pagesController.js
@@ -2,7 +2,15 @@ const pagesController = {
     'medios': function (req, res) {
 res.render('medios');
 
-    }
+    },
+    'costos': function (req, res) {
+        res.render('costos');
+        
+            },
+            'tiempos': function (req, res) {
+                res.render('tiempos');
+                
+                    }
 }
 
 module.exports = pagesController
--- site/routes/pagesRouter.js
+++ site/routes/pagesRouter.js
@@ -4,5 +4,9 @@ const pagesController = require('../controllers/pagesController')
 
 /* GET home page. */
 router.get('/medios-de-pago', pagesController.medios)
+router.get('/costos-de-envio', pagesController.costos)
+router.get('/tiempos-de-envio', pagesController.tiempos)
+
+
 
 module.exports = router;
